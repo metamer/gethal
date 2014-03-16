2b90acf40e7299a16b346f68799cad646e8ad3ed
metamer <metamer@openmailbox.org>
2014-03-16 18:32:28 -0400

    Added log messages and improved handling of noteye dirs. Added cleanup after noteye completes running

--- src/graphicalgame.cpp
+++ src/graphicalgame.cpp
@@ -6,14 +6,15 @@
 #include <iostream>
 #include <cstdlib>
 
-
 int main(int argc, char *argv[]){
+		std::cout<<"Start Graphical Game"<<std::endl;
 		std::vector<std::string> args(argv+1, argv+argc);
 
 		std::string noteye_dir;
 		std::string noteye_user_dir;
 		char* noteye_dir_env=std::getenv("NOTEYE_HOME");
 		char* noteye_user_dir_env=std::getenv("NOTEYE_USER_HOME");
+		char* user_home_dir_env=std::getenv("HOME");
 
 		//Set noteye config variables
 
@@ -21,16 +22,14 @@ int main(int argc, char *argv[]){
 		noteye_dir="/usr/share/noteye";
 		//location of config.noe
 
-		std::cout<<"argc="<<argc<<std::endl;	
-
 		if(argc>1 ){
 				noteye_dir=args[0];
 		}else if (noteye_dir_env!=0){
 				noteye_dir.assign(noteye_dir_env);	
 		}
 
-		std::cout<<"Setting noteye_dir to "<<noteye_dir<<std::endl;	
-		
+		std::cout<<"Setting noteye_dir to "<<noteye_dir<<std::endl;
+
 		//default to usersubdir of noteye_dir
 		noteye_user_dir=noteye_dir+"/user";
 
@@ -38,12 +37,21 @@ int main(int argc, char *argv[]){
 				noteye_user_dir=args[1];
 		}else if (noteye_user_dir_env!=0){
 				noteye_user_dir.assign(noteye_user_dir_env);	
+		}else if (user_home_dir_env!=0){
+				noteye_user_dir.assign(user_home_dir_env);
+				noteye_user_dir+=("/.config/noteye");
 		}
-
 		std::cout<<"Setting noteye_user_dir to "<<noteye_user_dir<<std::endl;	
 
 		ui::NotEyeUI sui = ui::NotEyeUI(noteye_dir,noteye_user_dir) ;
 		runner::SimpleRunner sr = runner::SimpleRunner(sui);
 
 		sr.run_game();
+		// tell the script that the game is finished
+		noteye::noteye_finishinternal(1);
+		// close the UI thread
+		noteye::noteye_uifinish();
+		// finish NotEye (this also exits the process)
+		noteye::noteye_halt();
+		std::cout<<"End Graphical Game"<<std::endl;
 }
